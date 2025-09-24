8394fee7b29dac73ae3deb8f0930560a7556dfff
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-09-24 18:24:35 -0400

    finished main flow of game, but it is very buggy lol

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,11 +24,34 @@ int flipCoin(void) {
     return result;
 }
 
+int placeGuess(void) {
+    char guess;
+    int format_guess;
+    int attempts = 0;
+
+    printf("[h]eads or [t]ails\n");
+    while (((tolower(guess) != 'h') || (tolower(guess) != 't')) && attempts < 3) {
+        guess = fgetc(stdin);
+        attempts++;
+    }
+
+    if (guess == 'h') {
+        format_guess = 1;
+        printf("You guess: Heads\n");
+    } else {
+        format_guess = 0;
+        printf("You guess: Tails\n");
+    }
+
+    return format_guess;
+}
+
 int main() {
     int player_money = START_AMOUNT;
-    int desired_bet, bet_amount, coin_flip;
+    int desired_bet, bet_amount, coin_flip, guess;
 
     while (player_money > 0) {
+        printf("Jollars: %s%d\n", JOLLAR_SIGN, START_AMOUNT);
         printf("Input desired bet: ");
         scanf("%d", &desired_bet);
 
@@ -35,9 +59,16 @@ int main() {
 
         printf("Bet Amount: %s%d\n", JOLLAR_SIGN, bet_amount);
 
+        guess = placeGuess();
         coin_flip = flipCoin();
-
-        printf("Heads or tails: %d\n", coin_flip);
+   
+        if (coin_flip != guess) {
+            printf("LOSS\n");
+            player_money -= bet_amount;
+        } else {
+            printf("WIN\n");
+            player_money += floor(player_money * ((bet_amount / player_money) + 1));
+        }
     }
 
     return 0;
